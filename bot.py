7f84a59c2ef95eff8b04e956f53335ecf0e18907
vasanthpk3 <35796747+vasanthpk3@users.noreply.github.com>
2018-02-20 22:01:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,8 +13,8 @@ from telegram.contrib.botan import Botan
 import python3pickledb as pickledb
 
 # Configuration
-BOTNAME = 'usernameofbot'
-TOKEN = 'TOKEN'
+BOTNAME = 'Welcome_Airdrops_bot'
+TOKEN = '483027669:AAHOhguMhKCeAdo4hhPqtqEPVZk5EitGltk'
 BOTAN_TOKEN = 'BOTANTOKEN'
 
 help_text = 'Welcomes everyone that enters a group chat that this bot is a ' \
